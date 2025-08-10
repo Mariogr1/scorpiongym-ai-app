0ee20ff3fe6998c22561126936cd5820b0199580
Mariogr1 <mariogr12011@gmail.com>
2025-08-10 17:22:30 -0300

    Refactor: bton de guardar cambios libreria independientes

--- src/index.tsx
+++ src/index.tsx
@@ -1292,7 +1292,6 @@ const ExerciseLibraryManager = ({ onSave, onBack }: { onSave: (library: Exercise
                                         onChange={(e) => setEditingText(e.target.value)}
                                         className="editing-input"
                                         autoFocus
-                                        onBlur={handleSaveEdit}
                                         onKeyDown={(e) => e.key === 'Enter' && handleSaveEdit()}
                                     />
                                 ) : (
